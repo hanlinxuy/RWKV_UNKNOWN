ffe0be519448dd3e4cb76e60db35affa5e29cdc3
Hanlin Xu <hanlinxu@mail.ustc.edu.cn>
2023-09-04 23:26:27 +0800

    add neromous' methods

---
+++ trainer/utils.py
@@ -0,0 +1,28 @@
+import os
+import json
+import re
+
+def sorted_fn(text):
+    n = re.findall(r'rwkv\-(.+?)\.', text)
+    n = n[0]
+    n = int(n)
+    return n
+
+
+def get_model(proj_path):
+    files = os.listdir(proj_path)
+    model_weights = [x for x in files if x.endswith('.pth')]
+    model_weights =  sorted(model_weights,key=sorted_fn)
+    model = model_weights[-1]
+    model = f'{proj_path}/{model}'
+    return model
+
+def next_model(proj_path):
+    files = os.listdir(proj_path)
+    model_weights = [x for x in files if x.endswith('.pth')]
+    model_weights =  sorted(model_weights,key=sorted_fn)
+    model = model_weights[-1]
+    n = re.findall(r'rwkv\-(.+?)\.', model)
+    n = n[0]
+    n = int(n)
+    return n + 1
